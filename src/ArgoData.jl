80d9c48f238cae4b99f0cc0ea218231d0b55f4d4
gaelforget <gforget@mit.edu>
2020-02-07 11:34:56 -0500

    convert time to a year axis + add label

--- src/ArgoData.jl
+++ src/ArgoData.jl
@@ -5,28 +5,27 @@ using NetCDF, Plots, Dates
 export MITprofDemo
 
 """
-    MITprofDemo()
+    MITprofDemo(f::String="MITprof/MITprof_mar2016_argo9506.nc")
 
 Standard Depth Argo Data Example.
 
 Here we use the `MITprof` data sets from [https://doi.org/10.7910/DVN/EE3C40]()
 For more information, please refer to [Forget, et al 2015] (http://dx.doi.org/10.5194/gmd-8-3071-2015)
-
-- [x] provide doi & ref for MITprof data set
-- [x] read in MITprof data using NCDatasets.jl
-- [x] plot number of profiles as function of time
+The produced figure shows the number of profiles as function of time for one file
 
 ```
-(f,i,h)=MITprofDemo()
+(f,h)=MITprofDemo()
 display(h)
 ```
 """
-function MITprofDemo()
-    f = "MITprof/MITprof_mar2016_argo9506.nc"
+function MITprofDemo(f::String="MITprof/MITprof_mar2016_argo9506.nc")
     i = ncinfo(f)
     x = ncread(f, "prof_date")
-    h = histogram(x,title="number of Argo profiles with time")
-    return f,i,h
+    t = julian2datetime.( datetime2julian(DateTime(0)) .+ x )
+    y = year.(t) + dayofyear.(t) ./ 365.0 #neglecting leap years ...
+    h = histogram(y,bins=20,label=f,title=
+        "number of Argo profiles with time")
+    return f,h
 end
 
 end # module
