1cb19ee0cc7323bf1b5744e94133ca0b082e26f7
gaelforget <gforget@mit.edu>
2020-04-19 16:48:40 -0400

    add MITprof_loop & remove Plots dependency

--- src/ArgoData.jl
+++ src/ArgoData.jl
@@ -1,8 +1,8 @@
 module ArgoData
 
-using NetCDF, Plots, Dates
+using NetCDF, Dates
 
-export MITprof_read
+export MITprof_read, MITprof_loop
 
 """
     MITprof_read(f::String="MITprof/MITprof_mar2016_argo9506.nc")
@@ -28,13 +28,34 @@ scatter(lo[kk],la[kk],label=fi[end-10:end],title="Argo profiles count")
 ```
 """
 function MITprof_read(f::String="MITprof/MITprof_mar2016_argo9506.nc")
-    i = ncinfo(f)
+    #i = ncinfo(f)
     lo = ncread(f, "prof_lon")
     la = ncread(f, "prof_lat")
     x = ncread(f, "prof_date")
     t = julian2datetime.( datetime2julian(DateTime(0)) .+ x )
     ye = year.(t) + dayofyear.(t) ./ 365.0 #neglecting leap years ...
-    return lo,la,ye
+    return Float64.(lo),Float64.(la),Float64.(ye)
+end
+
+"""
+    MITprof_loop(pth::String="profiles/")
+
+Standard Depth Argo Data Collection -- see `?MITprof_read` for detail.
+"""
+function MITprof_loop(pth::String="profiles/")
+    λ=("MITprof_mar2016_argo9506.nc","MITprof_mar2016_argo0708.nc",
+    "MITprof_mar2016_argo0910.nc","MITprof_mar2016_argo1112.nc",
+    "MITprof_mar2016_argo1314.nc","MITprof_mar2016_argo1515.nc")
+
+    lo=[]; la=[]; ye=[];
+    for f=1:length(λ)
+        (tmplo,tmpla,tmpye)=MITprof_read(pth*λ[f])
+        lo=cat(lo,tmplo,dims=1)
+        la=cat(la,tmpla,dims=1)
+        ye=cat(ye,tmpye,dims=1)
+    end
+
+    return Float64.(lo),Float64.(la),Float64.(ye)
 end
 
 end # module
