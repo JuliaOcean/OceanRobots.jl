a30be055f80bdaa01a40775b7758ae473d7d3c11
Documenter.jl <documenter@juliadocs.github.io>
2021-07-13 09:16:37 +0000

    build based on 177da30

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OceanRobots.jl","page":"Home","title":"OceanRobots.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This package is at a very early stage of development. Stay tuned ...","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MITprof,DownloadArgo]\nOrder   = [:type,:function]","category":"page"}]
+}
