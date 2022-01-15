432510615f4f4bb8cfc121da9f1e09f59a049018
gaelforget <gforget@mit.edu>
2022-01-15 12:47:40 -0500

    access hourly drifter data set via ftp, nectdf

--- src/read_data.jl
+++ src/read_data.jl
@@ -1,3 +1,67 @@
+
+"""
+    drifters_hourly_files()
+
+Get list of drifter files from NOAA ftp server     
+<ftp://ftp.aoml.noaa.gov/pub/phod/lumpkin/hourly/v1.04/netcdf/>
+"""
+function drifters_hourly_files()
+    list_files=DataFrame("folder" => [],"filename" => [])
+    list_folders=["gps",["argos_block$i" for i in 1:8]...]
+    for pth in list_folders
+        ftp=FTP("ftp://ftp.aoml.noaa.gov/pub/phod/lumpkin/hourly/v1.04/netcdf/")
+        cd(ftp,pth)
+        tmp=readdir(ftp)
+        append!(list_files,DataFrame("folder" => pth,"filename" => tmp))
+        cd(ftp,"..")
+    end
+    list_files
+end
+
+# 6-hourly interpolated data product
+# https://www.ncei.noaa.gov/access/metadata/landing-page/bin/iso?id=gov.noaa.nodc:AOML-GDP
+# url="https://www.ncei.noaa.gov/thredds-ocean/fileServer/aoml/gdp/1982/drifter_7702192.nc"
+
+"""
+    drifters_hourly_download(list_files,ii=1)
+
+Download one drifter file from NOAA ftp server     
+<ftp://ftp.aoml.noaa.gov/pub/phod/lumpkin/hourly/v1.04/netcdf/>
+
+```
+list_files=drifters_hourly_files()
+fil=drifters_hourly_download(list_files,1)
+```
+"""
+function drifters_hourly_download(list_files,ii=1)
+    url0="ftp://ftp.aoml.noaa.gov/pub/phod/lumpkin/hourly/v1.04/netcdf/"
+    url1=joinpath(url0,list_files[ii,"folder"])
+    ftp=FTP(url1)
+
+    fil=list_files[ii,"filename"]
+    
+    pth=joinpath(tempdir(),"drifters_hourly_noaa")
+    !isdir(pth) ? mkdir(pth) : nothing
+    fil_out=joinpath(pth,fil)
+
+    !isfile(fil_out) ? FTPClient.download(ftp, fil, fil_out) : nothing
+    fil_out
+end
+
+"""
+    drifters_hourly_download(list_files,ii=1)
+
+Download one drifter file from NOAA ftp server     
+<ftp://ftp.aoml.noaa.gov/pub/phod/lumpkin/hourly/v1.04/netcdf/>
+
+```
+list_files=drifters_hourly_files()
+fil=drifters_hourly_download(list_files,1)
+ds=drifters_hourly_read(fil)
+```
+"""
+drifters_hourly_read(filename::String)= Dataset(filename)
+
 """
     drifters_hourly_mat(pth,lst;chnk=Inf,rng=(-Inf,Inf))
 
@@ -72,7 +136,7 @@ pth,lst=drifters_hourly_mat()
 ```
 """
 function drifters_hourly_mat()
-    pth="Drifter_hourly_v014/"
+    pth=joinpath(tempdir(),"Drifter_hourly_v014")
     lst=["hourly_WMLE_1.02_block1.mat","hourly_WMLE_1.02_block2.mat",
         "hourly_WMLE_1.02_block3.mat","hourly_WMLE_1.02_block4.mat",
         "hourly_WMLE_1.02_block5.mat","hourly_WMLE_1.02_block6.mat",
@@ -87,11 +151,12 @@ function drifters_hourly_mat()
 Loop over all files and call drifters_hourly_mat with rng=(t0,t1)
 
 ```
-@everywhere using OceanRobots, CSV
+@everywhere using OceanRobots
 @distributed for y in 2005:2020
     df=drifters_hourly_mat(y+0.0,y+1.0)
-    fil="Drifter_hourly_v014/csv/drifters_"*string(y)*".csv"
-    CSV.write(fil, df)
+    pth=joinpath(tempdir(),"Drifter_hourly_v014","csv")
+    fil=joinpath("drifters_"*string(y)*".csv")
+    OceanRobots.CSV.write(fil, df)
 end
 ```
 """
