377959c94859dea9112ed39e3256ecf912424604
gaelforget <gforget@mit.edu>
2022-01-20 11:38:25 -0500

    add thredds_servers.jl to scan for files

---
+++ src/thredds_servers.jl
@@ -0,0 +1,52 @@
+
+"""
+    parse_thredds_catalog(url,recursive=true)
+
+Starting from an `xml` (not `html`) _thredds catalog_ look for both subfolders and files;
+they are identified based on the `href` and `urlPath` attributes respectively. If `recursive`
+is set to `true` then go down in subfolders and do the same until only files are found; in this
+case the returned `folders` should be empty and `files` can be extensive.
+
+See <https://www.unidata.ucar.edu/software/tds/current/catalog/> for more on `thredds`.
+
+```
+url="https://dods.ndbc.noaa.gov/thredds/catalog/oceansites/long_timeseries/WHOTS/catalog.xml"
+#url="https://dods.ndbc.noaa.gov/thredds/catalog/data/catalog.xml"
+#url="https://dods.ndbc.noaa.gov/thredds/catalog/oceansites-tao/catalog.xml"
+#url="https://dods.ndbc.noaa.gov/thredds/catalog/tao-ctd/catalog.xml"
+#url="https://dods.ndbc.noaa.gov/thredds/catalog/hfradar/catalog.xml"
+#url="https://dods.ndbc.noaa.gov/thredds/catalog/oceansites/catalog.xml"
+
+files,folders=parse_thredds_catalog(url)
+```
+"""
+function parse_thredds_catalog(url,recursive=true)
+    fil=Downloads.download(url)
+
+    xdoc=parse_file(fil);
+    xroot=root(xdoc);
+    e1=xroot["dataset"][1];
+    e2=collect(child_elements(e1));
+
+    files=String[]
+    folders=String[]
+    for i in e2
+        j=attributes_dict(i)
+        if haskey(j,"href")
+            push!(folders,j["href"])
+        elseif haskey(j,"urlPath")
+            push!(files,j["urlPath"])
+        end
+    end
+
+    if recursive
+        while !isempty(folders)
+            tmp=pop!(folders)
+            url0=joinpath(dirname(url),tmp)
+            tmp1,tmp2=parse_thredds_catalog(url0)
+            [push!(files,j) for j in tmp1]
+        end
+    end
+
+    return files,folders
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using OceanRobots
 using Test
 
 @testset "OceanRobots.jl" begin
-    # Write your own tests here.
+
+    url="https://dods.ndbc.noaa.gov/thredds/catalog/oceansites/long_timeseries/WHOTS/catalog.xml"
+    files,folders=parse_thredds_catalog(url)
+
+    @test isa(files[1],String)
+    @test isempty(folders)
+
+    list_files=drifters_hourly_files()
+    fil=drifters_hourly_download(list_files,1)
+    ds=drifters_hourly_read(fil)
+
+    @test isa(ds["longitude"],OceanRobots.NCDatasets.CFVariable)
 end
