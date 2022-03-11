0568d6fce89fe75576d18657d1060de1a433e75a
gaelforget <gforget@mit.edu>
2022-03-11 07:36:57 -0500

    another try

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,10 @@ using Test
 
     list_files=drifters_hourly_files()
     fil=drifters_hourly_download(list_files,1)
+    println("fil = $(fil)")
     ds=drifters_hourly_read(fil)
+    typ=typeof(ds)
+    println("ds = $(typ)")
 
     @test isa(ds["longitude"],OceanRobots.NCDatasets.CFVariable)
 
