cd87f825dbd035f5d914e55c4c5cd6f43aeb4348
gaelforget <gforget@mit.edu>
2022-04-03 16:57:33 -0400

    update after code migration to ArgoData.jl

--- src/OceanRobots.jl
+++ src/OceanRobots.jl
@@ -7,7 +7,6 @@ include("read_data.jl")
 include("thredds_servers.jl")
 
 export drifters_hourly_files, drifters_hourly_download, drifters_hourly_read, drifters_hourly_mat
-export Argo_float_files, Argo_float_download
 export parse_thredds_catalog
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,17 +16,4 @@ using Test
     @test isa(ds["longitude"],OceanRobots.NCDatasets.CFVariable)
     @test isa(ds,OceanRobots.NCDataset)
 
-    fil="https://gaelforget.github.io/OceanRobots.jl/dev/examples/Argo_float_files.csv"
-    Argo_float_files(fil)
-
-    ii=10000
-    list_files=Argo_float_files()
-    Argo_float_download(list_files,ii)
-    ftp="ftp://usgodae.org/pub/outgoing/argo/dac/"
-    Argo_float_download(list_files,ii,"meta",ftp)
-
-    wmo=list_files[ii,"wmo"]
-    path=joinpath(tempdir(),"Argo_DAC_files",list_files[ii,"folder"])
-    fil=joinpath(path,string(wmo),string(wmo)*"_meta.nc")
-    @test isfile(fil)
 end
