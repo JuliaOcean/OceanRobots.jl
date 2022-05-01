96434c03c6172ffe043f9d7723af97420f62d99c
gaelforget <gforget@mit.edu>
2022-05-01 11:47:22 -0400

    fix path issue

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ OceanRobots.check_for_file("Spray_Glider","CUGN_along.nc")
 lst=("Spray_Glider.jl","Buoy_NWP_NOAA.jl","Mooring_WHOTS.jl","Drifter_GDP.jl","Float_Argo.jl")
 for i in lst
     fil_in=joinpath(@__DIR__,"..", "examples",i)
-    fil_out=joinpath(@__DIR__,"build", "examples",i[1:end-2]*"html")
+    fil_out=joinpath(@__DIR__,"build", i[1:end-2]*"html")
     PlutoSliderServer.export_notebook(fil_in)
     mv(fil_in[1:end-2]*"html",fil_out)
     cp(fil_in,fil_out[1:end-4]*"jl")
