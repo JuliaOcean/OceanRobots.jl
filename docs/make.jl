5a1e21ac1e93e77f5c4fe25cefb3957ea86b38a7
gaelforget <gforget@mit.edu>
2023-01-04 19:09:00 -0500

    leave download to notebook

--- docs/make.jl
+++ docs/make.jl
@@ -9,18 +9,6 @@ Pkg.build("PyCall")
 using Dataverse
 (DataAccessApi,NativeApi)=pyDataverse.APIs()
 
-DOI="doi:10.7910/DVN/OYBLGK"
-df=pyDataverse.dataset_file_list(DOI)
-lst=DataverseDownloads.download_urls(df)
-
-filenames=["exportImage_60arc.tiff","sla_podaac.nc"]
-pth0=joinpath(tempdir(),"azores_region_data")
-for filename in filenames
- file0=joinpath(pth0,filename)
- !ispath(pth0) ? mkdir(pth0) : nothing
- !isfile(file0) ? DataverseDownloads.download_files(lst,filename,pth0) : nothing
-end
-
 ##
 
 makedocs(;
