56b5162f035530e53454ef73ae42354bebef4154
gaelforget <gforget@mit.edu>
2024-05-25 00:35:39 -0400

    add coast lines, improve plots

--- ext/OceanRobotsMakieExt.jl
+++ ext/OceanRobotsMakieExt.jl
@@ -162,28 +162,35 @@ function plot_profiles!(ax,TIME,PRES,TEMP,cmap)
 	sca
 end
 
-function plot_trajectory!(ax,lon,lat,dx)
+function plot_trajectory!(ax,lon,lat,dx;
+		markersize=2,pol=Any[],xlims=(-180,180),ylims=(-90,90))
 	dt=10.0*86400
 	co=(dx[2:end]+dx[1:end-1])/2
 	co=[dx[1];co[:];dx[end]]/dt
 
 	li=lines!(ax,lon, lat, linewidth=2, color=co, colormap=:turbo)
-	scatter!(ax,lon, lat, marker=:circle, markersize=2, color=:black)
+	scatter!(ax,lon, lat, marker=:circle, markersize=markersize, color=:black)
 	ax.xlabel="longitude"
 	ax.ylabel="latitude"
-	ax.title="positions (dots) & speed (color)"
+	ax.title="estimated speed (m/s)"
+
+	!isempty(pol) ? [lines!(ax,l1,color = :black, linewidth = 0.5) for l1 in pol] : nothing
+
+	xlims!(xlims...); ylims!(ylims...)
 
 	li
 end
 
-function plot_standard(wmo,arr,spd,T_std,S_std)
-	fig1=Figure()
+function plot_standard(wmo,arr,spd,T_std,S_std;
+		markersize=2,pol=Any[],xlims=(-180,180),ylims=(-90,90))
+	fig1=Figure(size=(900,1200))
 	
 	ax=Axis(fig1[1,1])
-	li=plot_trajectory!(ax,arr.lon,arr.lat,spd.dx)
+	li=plot_trajectory!(ax,arr.lon,arr.lat,spd.dx;
+		markersize=2,pol=pol,xlims=xlims,ylims=ylims)
 	Colorbar(fig1[1,2], li, height=Relative(0.65))
-	ax=Axis(fig1[1,3],title="Float wmo="*string(wmo))
-	scatter!(ax,arr.PSAL[:],arr.TEMP[:],markersize=3.0)
+	ax=Axis(fig1[1,3],title="Float wmo="*string(wmo),xlabel="Salinity",ylabel="Temperature")
+	scatter!(ax,arr.PSAL[:],arr.TEMP[:],markersize=2.0)
 
 	ax=Axis(fig1[2,1:3],title="Temperature, °C")
 	hm1=heatmap_profiles!(ax,arr.TIME,T_std,:thermal)
